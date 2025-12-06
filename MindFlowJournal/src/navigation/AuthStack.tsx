eeec2f6375cbb55a48fd1ca0bbac249e0bec42b4
Jitendra Kumar Sahu <jitendrasahu6761@gmail.com>
2025-12-06 13:15:05 +0530

    update routing to launch sign up screen first

--- MindFlowJournal/src/navigation/AuthStack.tsx
+++ MindFlowJournal/src/navigation/AuthStack.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useEffect, useState } from 'react';
+import ForgotPasswordScreen from '../screens/Auth/ForgotPasswordScreen';
 import LoginScreen from '../screens/Auth/LoginScreen';
 import SignupScreen from '../screens/Auth/SignupScreen';
-import ForgotPasswordScreen from '../screens/Auth/ForgotPasswordScreen';
+import { isFirstLaunch } from '../services/unifiedStorageService';
 
 const Stack = createNativeStackNavigator();
 
 export const AuthStack: React.FC = () => {
+
+  const [isFirstTime, setIsFirstTime] = useState(false);
+  
+ useEffect(() => {
+    const initializeLoginState = async () => {
+      try {
+        // Check first launch
+        setIsFirstTime( await isFirstLaunch());
+
+      } catch (error) {
+        console.error("❌ init failed:", error);        
+      }
+    };
+
+    initializeLoginState();
+  }, []);
+
   return (
-    <Stack.Navigator
+    <Stack.Navigator    
+    initialRouteName="Signup"
       screenOptions={{
         headerShown: false,
       }}
     >
+      <Stack.Screen name="Signup" component={SignupScreen} /> 
       <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Signup" component={SignupScreen} />
       <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
     </Stack.Navigator>
   );
